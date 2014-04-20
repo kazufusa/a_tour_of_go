6484ed931a24e13d684d5b9e03414bd6327db280
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2014-04-20 20:50:40 +0900

    Fixed: #24

--- 24.go
+++ 24.go
@@ -6,8 +6,9 @@ import (
 
 func Sqrt(x float64) float64 {
 	z := float64(1)
+	fmt.Printf("2nd root of %v\n", z)
 	for i := 0; i < 10; i++ {
-		fmt.Println("loop:", i, "value:", z)
+		fmt.Printf("loop: %2d value: %v\n", i+1, z)
 		z = z - (z*z-x)/(2*z)
 	}
 
