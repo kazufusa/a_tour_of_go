db2cd3d38b7b1363044e744e8b97b57252ca7f81
Ishibashi Kazufusa <ishibashi@vic.co.jp>
2014-09-19 17:19:07 +0900

    [from now] 2014/09/19 17:19:07
     71.go | 44 +++++++++++++++++++++-----------------------
     1 file changed, 21 insertions(+), 23 deletions(-)

--- 71.go
+++ 71.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 type Fetcher interface {
@@ -10,46 +11,43 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
-type NextTargets struct {
-	urls  []string
-	depth int
-}
-
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 func Crawl(url string, depth int, fetcher Fetcher) {
 	// TODO: Fetch URLs in parallel.
 	// TODO: Don't fetch the same URL twice.
 	// This implementation doesn't do either:
-
-	c := make(chan NextTargets)
 	visited := map[string]bool{url: true}
+	wg := &sync.WaitGroup{}
 
-	worker := func(url string, depth int) {
+	worker := func(url string, depth int, fetcher Fetcher) {}
+	worker = func(url string, depth int, fetcher Fetcher) {
+		defer wg.Done()
+
+		if depth <= 0 {
+			return
+		}
+
 		body, urls, err := fetcher.Fetch(url)
 		if err != nil {
 			fmt.Println(err)
-		} else {
-			fmt.Printf("found: %s %q\n", url, body)
+			return
 		}
-		c <- NextTargets{urls, depth - 1}
-		return
-	}
 
-	nw := 1
-	go worker(url, depth)
-
-	for nw > 0 {
-		next := <-c
-		nw--
-		for _, url := range next.urls {
-			if depth > 0 && !visited[url] {
-				nw++
+		fmt.Printf("found: %s %q\n", url, body)
+		for _, url := range urls {
+			if visited[url] == false {
 				visited[url] = true
-				go worker(url, next.depth)
+				wg.Add(1)
+				go worker(url, depth-1, fetcher)
 			}
 		}
 	}
+
+	wg.Add(1)
+	go worker(url, depth, fetcher)
+	wg.Wait()
+	return
 }
 
 func main() {
